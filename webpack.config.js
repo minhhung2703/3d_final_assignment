07cf6f7916ccca75f3782aecbdf6a2825e4b4d93
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2024-01-14 13:32:48 +0700

    fixing run time

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./docs"),
+    filename: "index_bundle.js",
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  devServer: {
+    contentBase: path.join(__dirname, "docs"),
+    open: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot)$/,
+        use: "file-loader?name=fonts/[name].[ext]!static",
+      },
+    ],
+  },
+};
